d9da8bec0812940e69e6b75de9af1139fb70301e
anthonyA1214 <anthonyamiluddin1@gmail.com>
2025-12-11 16:32:12 +0800

    changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
-import { type Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 import { env } from "~/env";
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: env.DATABASE_URL,
+    authToken: env.DATABASE_AUTH_TOKEN,
   },
   tablesFilter: ["aa_*"],
-} satisfies Config;
+});
